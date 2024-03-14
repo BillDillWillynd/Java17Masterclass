d5a8012fea6b13964b8fd17c9573d6d5de6ae1f8
BillDillWillynd <dwandrewsno1@gmail.com>
2024-03-14 22:04:49 +0000

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
 
     public static void main(String[] args){
 
-        EnhancedPlayer tim = new EnhancedPlayer("Tim");
-        System.out.println("Initial health is: " + tim.healthRemaining());
-
-        EnhancedPlayer john = new EnhancedPlayer("John", 200, "Staff");
-        System.out.println("Initial health is: " + john.healthRemaining());
+        Movie movie = Movie.getMovie("ScienceFiction", "Star Wars");
+        movie.watchMovie();
     }
 
 }
